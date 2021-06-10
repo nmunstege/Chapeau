ba42f6663852e2f16fe8fe0524fa6323cfc44adb
mouradjwz <664995@student.inholland.nl>
2021-06-10 17:06:04 +0200

    Started kitchen UI

--- ChapeauUI/Program.cs
+++ ChapeauUI/Program.cs
@@ -20,8 +20,8 @@ namespace ChapeauUI
             Application.SetCompatibleTextRenderingDefault(false);
             Order order = new Order();
             //Application.Run(new OrderingUI(new Order(1, 1, 0, 1)));
-            Application.Run(new Payment(new Order(1, 1, 0, 1)));
-
+            //Application.Run(new Payment(new Order(1, 1, 0, 1)));
+            Application.Run(new KitchenUI());
         }
     }
 }
